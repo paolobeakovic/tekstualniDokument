1186482e699c972b71d2b299dc5b11fcd9353a07
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-05-26 12:08:09 +0200

    Add files via upload

---
+++ beakovicPaoloTekstualniDokument/beakovicPaoloTekstualniDokument/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace beakovicPaoloTekstualniDokument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = @"C:\Users\Paolo\Documents\";
+            string fileName = "CSharpCornerAuthors.txt";
+            string fullPath = folder + fileName;
+
+            string[] authors = { "Mahesh Chand", "Allen O'Neill", "David McCarter", "Raj Kumar", "Dhananjay Kumar" };
+            File.WriteAllLines(fullPath, authors);
+            string readText = File.ReadAllText(fullPath);
+            Console.WriteLine(readText);
+
+            Console.ReadKey();
+        }
+    }
+}
